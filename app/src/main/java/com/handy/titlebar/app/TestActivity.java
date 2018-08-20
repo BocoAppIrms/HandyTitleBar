eb237e3ac3451562200309bfac0e3009fa0608b1
liujie <liujie045.china@outlook.com>
2018-08-20 17:02:41 +0800

    Handy Commit

--- app/src/main/java/com/handy/titlebar/app/TestActivity.java
+++ app/src/main/java/com/handy/titlebar/app/TestActivity.java
@@ -1,59 +1,52 @@
 package com.handy.titlebar.app;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-import com.handy.titlebar.TitleBar;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.handy.titlebar.HandyTitleBar;
 
 public class TestActivity extends AppCompatActivity {
-
-    @BindView(R.id.main_dynamic)
-    Button mainDynamic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        ButterKnife.bind(this);
 
-        /* 初始化标题栏 */
-        TitleBar titleBar = findViewById(R.id.base_mytitlebar);
-        titleBar.setTitle("MyTitleBar 使用样例");
-        titleBar.setImmersive(TestActivity.this, true);
-        titleBar.setBackgroundColor(Color.parseColor("#1f1f1f"));
-        titleBar.addLeftAction(new TitleBar.Action() {
+        final HandyTitleBar handyTitleBar1 = findViewById(R.id.titlebarNew);
+        handyTitleBar1.showCustomStatusBar(TestActivity.this);
+        handyTitleBar1.setTitleBarContent("主标题\n副标题");
+        handyTitleBar1.addLeftAction(new HandyTitleBar.BaseAction(handyTitleBar1) {
+            {
+                this.setImageSrc(R.drawable.hdb_back_n);
+                this.setText(null);
+                this.setTextColor(Color.WHITE);
+                this.setTextSize(15);
+                this.setImageSize(10);
+            }
+
             @Override
             public void onClick() {
                 finish();
             }
+        });
+        handyTitleBar1.addRightAction(new HandyTitleBar.BaseAction(handyTitleBar1) {
+            {
+                this.setImageSrc(R.drawable.hdb_menu_n);
+            }
 
             @Override
-            public String setText() {
-                return "退出";
+            public void onClick() {
             }
         });
-    }
+        handyTitleBar1.addRightAction(new HandyTitleBar.BaseAction(handyTitleBar1) {
+            {
+                this.setImageSrc(R.drawable.hdb_setting_n);
+            }
 
-    @OnClick({R.id.main_dynamic, R.id.main_newtitlebar})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.main_dynamic:
-                Intent intent = new Intent(TestActivity.this, TitleBarTestActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.main_newtitlebar:
-                Intent intent1 = new Intent(TestActivity.this, NewTitleBarTestActivity.class);
-                startActivity(intent1);
-                break;
-            default:
-        }
+            @Override
+            public void onClick() {
+            }
+        });
     }
 }
